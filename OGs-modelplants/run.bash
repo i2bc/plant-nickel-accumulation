9c0f677cc7f600b2bfb1c653ea95a0b4c459b494
christine.drevet <christine.drevet@i2bc.paris-saclay.fr>
2020-11-12 18:11:27 +0100

    Changes to be committed: 	new file:   mario_converted/.gitkeep 	new file:   mario_input/.gitkeep 	new file:   mariodb/.gitkeep 	new file:   modelsblast/.gitkeep 	new file:   modelsfasta/.gitkeep 	new file:   numblast/.gitkeep 	new file:   numfasta/.gitkeep 	new file:   numfiles/.gitkeep

--- OGs-modelplants/run.bash
+++ OGs-modelplants/run.bash
@@ -1,29 +1,31 @@
-Fichiers source de l'annotation:
-swissprot version 24 : <gitclonedir>/annotations/annot_source/uniprot_sprot.dat et <gitclonedir>/annotations/annot_source/uniprot_sprot.xml, uniprot_sprot.dat
-metacyc version 20.1 : dossier 20.1 dans <gitclonedir>/annotations/annot_source/
 
-Fichier de reference des EC numbers et des GO entries:
-perl 8_write_reference_files.pl <gitclonedir> <version du dossier de metacyc> 8_intersection.out
+We download and store in the OGs-modelplants/fasta directory the fasta formatted proteomes
+We download and store in the OGs-modelplants/annotations/annot_source/ the thereafter ressources
+swissprot version 24 (ftp://ftp.uniprot.org) : OGs-modelplants/annotations/annot_source/uniprot_sprot.dat et OGs-modelplants/annotations/annot_source/uniprot_sprot.xml, uniprot_sprot.dat
+metacyc version 20.1 (https://metacyc.org) : OGs-modelplants/annotations/annot_source/20.1
+
+Building reference files for EC numbers and GO entries:
+perl 8_write_reference_files.pl OGs-modelplants <version du dossier de metacyc> 8_intersection.out
 output files:
-<gitclonedir>/annotations/annot_files/SwissProt_ec.sql
-<gitclonedir>/annotations/annot_files/SwissProt_go.sql
-<gitclonedir>/annotations/annot_files/MetaCyc_ec.sql
+OGs-modelplants/annotations/annot_files/SwissProt_ec.sql
+OGs-modelplants/annotations/annot_files/SwissProt_go.sql
+OGs-modelplants/annotations/annot_files/MetaCyc_ec.sql
 
-<gitclonedir>/annotations/annot_files/SwissProt_MetaCyc_ec.sql
-<gitclonedir>/annotations/annot_files/SwissProt_MetaCyc_go.sql
+OGs-modelplants/annotations/annot_files/SwissProt_MetaCyc_ec.sql
+OGs-modelplants/annotations/annot_files/SwissProt_MetaCyc_go.sql
 
-<gitclonedir>/annotations/annot_files/SwissProt_MetaCyc_ec.fa
-<gitclonedir>/annotations/annot_files/SwissProt_MetaCyc_go.fa
+OGs-modelplants/annotations/annot_files/SwissProt_MetaCyc_ec.fa
+OGs-modelplants/annotations/annot_files/SwissProt_MetaCyc_go.fa
 
-Fichier de reference des fonctions swissprot (parsing uniprot_sprot.xml)
+Building reference files for swissprot functions (parsing uniprot_sprot.xml)
 python 10A_parse_function_uniprot.py
 output file:
-<gitclonedir>/annotations/annot_files/swissprot.fa 
+OGs-modelplants/annotations/annot_files/swissprot.fa 
 
-Fichier de reference des fonctions metacyc
-perl 8_parse_proteins_dat.pl <gitclonedir> 20.1 > annot_files/parsing_proteins_metacyc
+Building reference files for metacyc functions:
+perl 8_parse_proteins_dat.pl OGs-modelplants 20.1 > annot_files/parsing_proteins_metacyc
 perl 8_parse_proteins_links_dat.pl <workdir> <metacyc version> > annot_files/metacyc_proteins_links> 
-perl 10B_write_functions_references.pl <gitclonedir> 
+perl 10B_write_functions_references.pl OGs-modelplants 
 output files:                    
 annot_files/metacyc.fa 
                                           
