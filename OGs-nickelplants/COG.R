afebc90b7b727b91a7e646bbf094a5b52ad0f310
christine.drevet <christine.drevet@i2bc.paris-saclay.fr>
2020-11-18 16:32:54 +0100

    mise a jour de COG.R (derniere version des espèces)

--- OGs-nickelplants/COG.R
+++ OGs-nickelplants/COG.R
@@ -1,7 +1,7 @@
 #!/usr/bin/Rscript
 rm(list = ls())
 #
-G = c("Gpru", "Hbet","LhavRL", "Mper","Pcos", "Psem", "Grac", "Hkan", "Lmic","Nmon", "Pcle", "Pgab", "Pgra","Noccaea","AT")
+G = c("Scor" ,"Gpru", "Grac", "Hbet", "Hkan","Mper", "Ncfi", "Nmon", "Pcos", "Pgab", "Pgra", "Prev", "Phco", "Phlu", "Psem", "ath")
 maxcontig <- vector("numeric", length(G))
 mediancontig <- vector("numeric", length(G))
 maxorf <- vector("numeric", length(G))
@@ -11,34 +11,43 @@ nb <- vector("numeric", length(G))
 for (i in 1:length(G))
 {
 	print(G[i])
-	file = paste("/home/chris/COGs/",G[i],".tab",sep = "")
+	file = paste("../../NickelPlants_data_article/COGs/orf_tab/",G[i],".tab",sep = "")
 	print(file)
 	A <- as.data.frame(read.table(file, sep = "\t"))
 	# longueur des contigs
 	A[,2] ->  l_contig
 	l_contig = as.numeric(l_contig)
 	# longueur de la plus grande ORF
-	A[,3] ->  l_orf
+	A[,4] ->  l_orf
 	l_orf = as.numeric(l_orf)
 
 	# diagramme des valeurs observées
-
-	hist(l_contig, breaks = 100, main=paste("genome",G[i],sep=" of "),xlim=c(0,max(l_contig)),ylim=c(0,16000), col=rgb(0,1,0,1))  # first histogram
-	par(new=TRUE)
-	hist(l_orf, breaks = 100, main=paste("genome",G[i],sep=" of "),xlim=c(0,max(l_contig)),col=rgb(1,0,0,1),ylim=c(0,16000))
+	#hist(l_contig, breaks = 100, main=paste("genome",G[i],sep=" of "),xlim=c(0,max(l_contig)),ylim=c(0,16000), col=rgb(0,1,0,1))  # first histogram
+	#par(new=TRUE)
+	#hist(l_orf, breaks = 100, main=paste("genome",G[i],sep=" of "),xlim=c(0,max(l_contig)),col=rgb(1,0,0,1),ylim=c(0,16000))
 	
 	maxcontig[i] =  max(l_contig)
+	print (c("contigs maximum length: ", maxcontig[i]))
 	mediancontig[i] = median(l_contig)
+	print (c("contigs median length: ",mediancontig[i]))
 	maxorf[i] =  max(l_orf)
+	print (c("ORF maximum length: ", maxorf[i]))
 	medianorf[i] = median(l_orf)
+	print (c("ORF median length: ",medianorf[i]))
 	nb[i] = length(l_contig)	
-	#mean(l_orf)
-	#sd(l_orf)	
+	print (c("number of contigs: ", nb[i]))
+
 }
 A<-gl(15,15,15,labels=G)
-data <-cbind(mediancontig,medianorf)
-rownames(data) <- levels(A)
-#barplot(mediancontig,names.arg=levels(A))
-barplot(t(data),beside=T,legend.text=colnames(data),
-col=c("grey50","grey80"),ylab="longueur(nt)")
+mat <-cbind(mediancontig,medianorf)
+rownames(mat) <- levels(A)
+
+#save(t(mat),file="mat_speci.RData")
+barplot(t(mat), beside = TRUE, main = "", horiz=F, col=c("grey50","grey80"),  xlab = "Species",cex.names=1, las=2, ylab="length (nt)")
+legend("center", 
+       legend = c("Contigs","ORFs"), 
+       fill = c("grey50","grey80"), ncol = 2,
+       cex = 0.75)
+
+
 warnings()
